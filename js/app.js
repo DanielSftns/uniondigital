bbb1c598ec919b79bbe723544d6c8d1c16e9fce1
Daniel Sifontes <danielsifontes99@gmail.com>
2020-07-24 15:30:02 -0400

    btn limites debug

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+const sections = document.querySelectorAll('.panel.section')
+const paquetes = document.querySelectorAll('.panel.section-paquetes')
+let limitesStatus = false
+document.querySelector('#limites').addEventListener('click',()=>{
+  if(limitesStatus){
+    sections.forEach(section=>{
+      section.style.backgroundColor = "#000e08"
+    })
+    paquetes.forEach(paquete=>{
+      paquete.style.backgroundColor = "#000e08"
+    })
+  }else{
+    sections.forEach(section=>{
+      section.style.backgroundColor = "red"
+    })
+    paquetes.forEach(paquete=>{
+      paquete.style.backgroundColor = "blue"
+    })
+  }
+  limitesStatus = !limitesStatus
+})
+
 const controller = new ScrollMagic.Controller();
 
 const tl = gsap.timeline()
@@ -5,13 +27,13 @@ const tl = gsap.timeline()
 tl.fromTo('.pinMaster1 .panel.section-paquetes', 1,
   { xPercent: 100 },
   { xPercent: 0, ease: Linear.easeNone },
-  "+=1"
+  "+=0"
 )
 
 new ScrollMagic.Scene({
   triggerElement: ".pinMaster1",
   triggerHook: "onLeave",
-  duration: "100%"
+  duration: "200%"
 })
   .setPin(".pinMaster1")
   .setTween(tl)
@@ -29,18 +51,18 @@ const tl2 = gsap.timeline()
 tl2.fromTo('.pinMaster2 .panel.section', 1,
   { xPercent: 0 },
   { xPercent: -100, ease: Linear.easeNone },
-  "+=1"
+  "+=0.5"
 )
 tl2.fromTo('.pinMaster2 .panel.section-paquetes', 1,
   { xPercent: 100 },
   { xPercent: 0, ease: Linear.easeNone },
-  "+=1"
+  "-=1"
 )
 
 new ScrollMagic.Scene({
   triggerElement: ".pinMaster2",
   triggerHook: "onLeave",
-  duration: "100%"
+  duration: "200%"
 })
   .setPin(".pinMaster2")
   .setTween(tl2)
