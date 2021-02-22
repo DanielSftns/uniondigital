8bcca4d043a2f181b142d11ce345de4b5f73a40b
daniel <danielsifontes99@gmail.com>
2021-02-22 19:58:04 -0400

    nuevo loader y lazyloading

---
+++ js/lazyLoading.js
@@ -0,0 +1,18 @@
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+            entry.target.src = entry.target.dataset.src;
+            // entry.target.addEventListener('load', function () {
+            //     entry.target.classList.remove('lazy-img');
+            // });
+        }
+    );
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
